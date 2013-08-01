71337db49f292f5e6f21428f346c5403cfc8e4c0
David <admin@dav1d.de>
2013-08-01 15:14:33 +0200

    main: read gl.xml if it exists

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from glad.generator import DGenerator
 import os.path
 
 def main():
-    spec = glad.parse.OpenGLSpec.from_file('gl.xml')
-    #spec = glad.parse.OpenGLSpec.from_opengl()
+    spec = None
+    if os.path.exists('gl.xml'):
+        spec = glad.parse.OpenGLSpec.from_file('gl.xml')
+    else:
+        spec = glad.parse.OpenGLSpec.from_opengl()
 
     gen = DGenerator(os.path.join('.', 'build'))
     gen.generate(spec, 'gl', (4, 3), 'core')
