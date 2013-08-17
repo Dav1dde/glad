96b2523555d15cbbe7e6720267562508ba0895a2
David <admin@dav1d.de>
2013-08-18 01:24:21 +0200

    loader: fix GL c has_ext for GL < 3

--- glad/loader/gl/c.py
+++ glad/loader/gl/c.py
@@ -35,8 +35,26 @@ _OPENGL_HAS_EXT = '''
 static int has_ext(const char *ext) {
     if(GLVersion.major < 3) {
         const char *extensions;
+        const char *loc;
+        const char *terminator;
         extensions = (const char *)glGetString(GL_EXTENSIONS);
-        return extensions != NULL && ext != NULL && strstr(extensions, ext) != NULL;
+        if(extensions == NULL || ext == NULL) {
+            return 0;
+        }
+
+        while(1) {
+            loc = strstr(extensions, ext);
+            if(loc == NULL) {
+                return 0;
+            }
+
+            terminator = loc + strlen(ext);
+            if((loc == extensions || *(loc - 1) == ' ') &&
+                (*terminator == ' ' || *terminator == '\\0')) {
+                return 1;
+            }
+            extensions = terminator;
+        }
     } else {
         int num;
         glGetIntegerv(GL_NUM_EXTENSIONS, &num);
