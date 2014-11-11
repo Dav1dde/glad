ba72a1075c3479b65f7443836d2d0c99b0cf56bd
Dav1dde <admin@dav1d.de>
2014-11-11 17:24:20 +0100

    tools: include -ansi in gcc compiletest option

--- compiletest.sh
+++ compiletest.sh
@@ -2,31 +2,34 @@
 
 set -e
 
+GCC_FLAGS="-Wall -Werror -ansi -c"
+GPP_FLAGS="-Wall -Werror -c"
+
 # C
 echo -e "=== Generating and compiling C/C++:"
 
 rm -rf build
 python main.py --generator=c --spec=egl --out-path=build
-gcc -Ibuild/include build/src/glad_egl.c -ldl -Wall -c
-g++ -Ibuild/include build/src/glad_egl.c -ldl -Wall -c
+gcc -Ibuild/include build/src/glad_egl.c ${GCC_FLAGS}
+g++ -Ibuild/include build/src/glad_egl.c ${GPP_FLAGS}
 
 
 rm -rf build
 python main.py --generator=c --spec=gl --out-path=build
-gcc -Ibuild/include build/src/glad.c -ldl -Wall -c
-g++ -Ibuild/include build/src/glad.c -ldl -Wall -c
+gcc -Ibuild/include build/src/glad.c ${GCC_FLAGS}
+g++ -Ibuild/include build/src/glad.c ${GPP_FLAGS}
 
 rm -rf build
 python main.py --generator=c --spec=gl --out-path=build
 python main.py --generator=c --spec=glx --out-path=build
-gcc -Ibuild/include build/src/glad_glx.c -ldl -Wall -c
-g++ -Ibuild/include build/src/glad_glx.c -ldl -Wall -c
+gcc -Ibuild/include build/src/glad_glx.c ${GCC_FLAGS}
+g++ -Ibuild/include build/src/glad_glx.c ${GPP_FLAGS}
 
 rm -rf build
 python main.py --generator=c --spec=gl --out-path=build
 python main.py --generator=c --spec=wgl --out-path=build
-i686-w64-mingw32-gcc -Ibuild/include build/src/glad_wgl.c -Wall -c
-i686-w64-mingw32-g++ -Ibuild/include build/src/glad_wgl.c -Wall -c
+i686-w64-mingw32-gcc -Ibuild/include build/src/glad_wgl.c ${GCC_FLAGS}
+i686-w64-mingw32-g++ -Ibuild/include build/src/glad_wgl.c ${GPP_FLAGS}
 
 # D
 echo -e "\n=== Generating and compiling D:"
