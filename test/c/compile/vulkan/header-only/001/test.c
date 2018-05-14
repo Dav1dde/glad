62ade721fb5aac33a2f7e47d2ff36d8228112783
David Herberth <admin@dav1d.de>
2018-05-15 00:05:38 +0200

    test: added vk header-only tests (the ones I previously forgot).

---
+++ test/c/compile/vulkan/header-only/001/test.c
@@ -0,0 +1,19 @@
+/*
+ * Full Vulkan header only
+ *
+ * GLAD: $GLAD --out-path=$tmp --api="vulkan" c --loader --header-only
+ * COMPILE: $GCC $test -o $tmp/test -I$tmp/include -ldl
+ * RUN: $tmp/test
+ */
+
+#define GLAD_VULKAN_IMPLEMENTATION
+#include <glad/vulkan.h>
+
+#if !defined(VK_EXT_debug_report) || !defined(VK_VERSION_1_0)
+#error
+#endif
+
+int main(void) {
+    (void) gladLoadVulkanInternalLoader(NULL, NULL, NULL);
+    return 0;
+}
---
+++ test/c/compile/vulkan/header-only/002/test.c
@@ -0,0 +1,19 @@
+/*
+ * Full Vulkan without extensions, header only
+ *
+ * GLAD: $GLAD --out-path=$tmp --api="vulkan" --extensions="" c --loader --header-only
+ * COMPILE: $GCC $test -o $tmp/test -I$tmp/include -ldl
+ * RUN: $tmp/test
+ */
+
+#define GLAD_VULKAN_IMPLEMENTATION
+#include <glad/vulkan.h>
+
+#if defined(VK_EXT_debug_report) || !defined(VK_VERSION_1_0)
+#error
+#endif
+
+int main(void) {
+    (void) gladLoadVulkanInternalLoader(NULL, NULL, NULL);
+    return 0;
+}
