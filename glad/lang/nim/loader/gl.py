00fb9287757364737277eec507e70f5fca743040
John Novak <john@johnnovak.net>
2019-11-17 16:53:15 +1000

    nim: mark exported functions as gcsafe

--- glad/lang/nim/loader/gl.py
+++ glad/lang/nim/loader/gl.py
@@ -106,7 +106,7 @@ _FIND_VERSION = '''  # Thank you @elmindreda
 
 '''
 
-_BEGIN_LOAD = '''  glGetString = cast[proc (name: GLenum): ptr GLubyte {.cdecl.}](load("glGetString"))
+_BEGIN_LOAD = '''  glGetString = cast[proc (name: GLenum): ptr GLubyte {.cdecl, gcsafe.}](load("glGetString"))
   if glGetString == nil: return false
 
   var glVersion = cast[cstring](glGetString(GL_VERSION))
