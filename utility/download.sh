1a3611f53cef41d7d6cbf160ee316226da0dee2c
David Herberth <github@dav1d.de>
2024-02-04 14:52:48 +0100

    files: updates to latest specifications

--- utility/download.sh
+++ utility/download.sh
@@ -30,16 +30,25 @@ wget -O "${TARGET}/eglplatform.h" https://raw.githubusercontent.com/KhronosGroup
 rm -f "${TARGET}/vk_platform.h"
 wget -O "${TARGET}/vk_platform.h" https://raw.githubusercontent.com/KhronosGroup/Vulkan-Docs/main/include/vulkan/vk_platform.h
 
+rm -f "${TARGET}/vulkan_video_codecs_common.h"
+wget -O "${TARGET}/vulkan_video_codecs_common.h" https://raw.githubusercontent.com/KhronosGroup/Vulkan-Headers/main/include/vk_video/vulkan_video_codecs_common.h
+
 rm -f "${TARGET}/vulkan_video_codec_h264std.h"
 wget -O "${TARGET}/vulkan_video_codec_h264std.h" https://raw.githubusercontent.com/KhronosGroup/Vulkan-Headers/main/include/vk_video/vulkan_video_codec_h264std.h
 rm -f "${TARGET}/vulkan_video_codec_h264std_decode.h"
 wget -O "${TARGET}/vulkan_video_codec_h264std_decode.h" https://raw.githubusercontent.com/KhronosGroup/Vulkan-Headers/main/include/vk_video/vulkan_video_codec_h264std_decode.h
 rm -f "${TARGET}/vulkan_video_codec_h264std_encode.h"
-wget -O "${TARGET}/vulkan_video_codec_h264std_encode.h" https://raw.githubusercontent.com/KhronosGroup/Vulkan-Headers/main/include/vk_video/vulkan_video_codec_h264std_decode.h
+wget -O "${TARGET}/vulkan_video_codec_h264std_encode.h" https://raw.githubusercontent.com/KhronosGroup/Vulkan-Headers/main/include/vk_video/vulkan_video_codec_h264std_encode.h
 
 rm -f "${TARGET}/vulkan_video_codec_h265std.h"
 wget -O "${TARGET}/vulkan_video_codec_h265std.h" https://raw.githubusercontent.com/KhronosGroup/Vulkan-Headers/main/include/vk_video/vulkan_video_codec_h265std.h
 rm -f "${TARGET}/vulkan_video_codec_h265std_decode.h"
 wget -O "${TARGET}/vulkan_video_codec_h265std_decode.h" https://raw.githubusercontent.com/KhronosGroup/Vulkan-Headers/main/include/vk_video/vulkan_video_codec_h265std_decode.h
 rm -f "${TARGET}/vulkan_video_codec_h265std_encode.h"
-wget -O "${TARGET}/vulkan_video_codec_h265std_encode.h" https://raw.githubusercontent.com/KhronosGroup/Vulkan-Headers/main/include/vk_video/vulkan_video_codec_h265std_decode.h
+wget -O "${TARGET}/vulkan_video_codec_h265std_encode.h" https://raw.githubusercontent.com/KhronosGroup/Vulkan-Headers/main/include/vk_video/vulkan_video_codec_h265std_encode.h
+
+
+rm -f "${TARGET}/vulkan_video_codec_av1std.h"
+wget -O "${TARGET}/vulkan_video_codec_av1std.h" https://raw.githubusercontent.com/KhronosGroup/Vulkan-Headers/main/include/vk_video/vulkan_video_codec_av1std.h
+rm -f "${TARGET}/vulkan_video_codec_av1std_decode.h"
+wget -O "${TARGET}/vulkan_video_codec_av1std_decode.h" https://raw.githubusercontent.com/KhronosGroup/Vulkan-Headers/main/include/vk_video/vulkan_video_codec_av1std_decode.h
