e9000a82ffe440f49a61137155e891ef62d3271d
David Herberth <github@dav1d.de>
2024-04-07 14:47:03 +0200

    revert include order

--- example/c/gl_glfw.c
+++ example/c/gl_glfw.c
@@ -1,10 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 #define GLAD_GL_IMPLEMENTATION // Necessary for headeronly version.
 #include <glad/gl.h>
 
 #include <GLFW/glfw3.h>
 
-#include <stdlib.h>
-#include <stdio.h>
 
 const GLuint WIDTH = 800, HEIGHT = 600;
 
