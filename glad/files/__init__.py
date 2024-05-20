59fd02e3d3bcebc710025aac164b30f0a73ae64f
David Herberth <github@dav1d.de>
2024-05-20 12:13:25 +0200

    files: lookup resources via importlib if available
    gh: #469

--- glad/files/__init__.py
+++ glad/files/__init__.py
@@ -2,13 +2,19 @@ import os.path
 import logging
 
 try:
-    from pkg_resources import resource_exists, resource_stream
-except ImportError:
-    def resource_exists(*args, **kwargs):
-        return False
+    from importlib.resources import files
 
-    def resource_stream(*args, **kwargs):
-        return None
+    def resource_open(package, name, *args, **kwargs):
+        return files(package).joinpath(name).open(*args, **kwargs)
+except ImportError:
+    try:
+        from pkg_resources import resource_stream
+
+        def resource_open(package, name, *args, **kwargs):
+            return resource_stream(package, name)
+    except ImportError:
+        def resource_open(package, name, *args, **kwargs):
+            raise FileNotFoundError
 
 
 BASE_PATH = os.path.abspath(os.path.dirname(__file__))
@@ -19,9 +25,11 @@ logger = logging.getLogger('glad.files')
 def open_local(name, *args, **kwargs):
     # use pkg_resources when available, makes it work in zipped modules
     # or other environments
-    if resource_exists(__name__, name):
+    try:
         logger.info('opening \'%s\' from packaged resource', name)
-        return resource_stream(__name__, name)
+        return resource_open(__name__, name, *args, **kwargs)
+    except FileNotFoundError:
+        pass
 
     # fallback to filesystem
     logger.info('opening \'%s\' from packaged path', name)
